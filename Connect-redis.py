8ef4f9d4fdb29741f6b91e969f431772e308c9ab
gordongekko67 <ensa77@yahoo.com>
2023-04-19 04:55:27 +0200

    redis

--- Connect-redis.py
+++ Connect-redis.py
@@ -1,10 +1,3 @@
-from rediscluster import RedisCluster
+import redis
+client = redis.StrictRedis(host='demo-redis2.5og6vn.clustercfg.euc1.cache.amazonaws.com:6379', port=6379)
 
-startup_nodes = [{"host": "demo-redis.5og6vn.ng.0001.euc1.cache.amazonaws.com", "port": "6379"}]
-
-# Note: decode_responses must be set to True when used with python3
-rc = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
-
-rc.set("foo", "bar")
-
-print(rc.get("foo"))
