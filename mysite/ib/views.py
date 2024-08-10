b48f2dfb8b73dba62f8b855f68ad35d4b74aa6ea
gordongekko6e <ensa77@yahoo.com>
2024-08-10 12:05:42 +0200

    git

--- mysite/ib/views.py
+++ mysite/ib/views.py
@@ -2094,7 +2094,7 @@ def analisi_dei_movimenti_anno(request):
     print(df)
     
     # prendo solo quelli che hanno il simbolo solo_mese uguale a 'AAPL 17FEB23'
-    df = df[df['Simbolo_solo_mese'] == 'SPY16AUG24']
+    df = df[df['Simbolo_solo_mese'] == 'MSFT20SEP24']
     
     # li ordino per data ora di esecuzione in modo ascendente
     df.sort_values(by=['Data/ora'], inplace=True, ascending=True)
